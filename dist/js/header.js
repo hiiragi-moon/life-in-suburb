3182d4a563b99ac775d6a3ff0a2bb8b1a7833558
新村柊月 <shimmurashuru@SMacBook.local>
2024-11-16 16:22:59 +0900

    ビルド

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,25 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.querySelector(".menu-button");
+document.addEventListener("DOMContentLoaded", function () {
+    // .btn-triggerと.menu-panelを取得
+    const button = document.querySelector(".btn-trigger");
     const menuPanel = document.querySelector(".menu-panel");
-    const menuIcon = document.querySelector(".menu-icon");
 
-    menuButton.addEventListener("click", () => {
-        menuPanel.classList.toggle("show"); // showクラスを付与/削除
-        const isOpen = menuIcon.classList.toggle("open");
-        // アイコンを切り替え
-        if (isOpen) {
-            menuIcon.innerHTML = `
-            <title>メニューを閉じる</title>
-            <path d="M6 6 L18 18 M6 18 L18 6" stroke="black" stroke-width="2" />
-        `;
+    // buttonまたはmenuPanelが存在しない場合は処理を終了
+    if (!button || !menuPanel) return;
+
+    // ボタンクリック時のイベントリスナー
+    button.addEventListener("click", function (event) {
+        // 'active'クラスをトグル
+        button.classList.toggle("active");
+
+        // menu-panelの表示/非表示を切り替え
+        if (menuPanel.style.transform === "translateY(0%)") {
+            menuPanel.style.transform = "translateY(-150%)";
         } else {
-            menuIcon.innerHTML = `
-            <title>メニューを開く</title>
-            <path
-                clip-rule="evenodd"
-                d="m4.25 8c0-.41421.33579-.75.75-.75h14c.4142 0 .75.33579.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.33579-.75-.75zm0 4c0-.4142.33579-.75.75-.75h14c.4142 0 .75.3358.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.3358-.75-.75zm.75 3.25c-.41421 0-.75.3358-.75.75s.33579.75.75.75h14c.4142 0 .75-.3358.75-.75s-.3358-.75-.75-.75z"
-                fill-rule="evenodd"></path>
-        `;
+            menuPanel.style.transform = "translateY(0%)";
         }
+
+        // デフォルト動作を防止
+        event.preventDefault();
     });
 });
